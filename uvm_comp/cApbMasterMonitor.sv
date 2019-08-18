0fa41d23703dae278032ccfe80535d612125e620
hoangviet81193 <53632249+hoangviet81193@users.noreply.github.com>
2019-08-18 17:41:00 +0700

    Update cApbMasterMonitor.sv
    Optimize code

--- uvm_comp/cApbMasterMonitor.sv
+++ uvm_comp/cApbMasterMonitor.sv
@@ -8,7 +8,7 @@ class cApbMasterMonitor extends uvm_monitor;
 	`uvm_component_utils(cApbMasterMonitor)
 
 	uvm_analysis_port #(cApbTransaction) ap_toScoreboard;
-    cApbTransaction coApbTransaction;
+    	cApbTransaction coApbTransaction;
 
 	virtual interface ifApbMaster vifApbMaster;
   virtual interface ifInterrupt vifInterrupt;
@@ -47,33 +47,40 @@ class cApbMasterMonitor extends uvm_monitor;
     // 4. Send configuration information
     virtual task collect_data( );
 	forever begin
-	wait(vifApbMaster.psel)
-	coApbTransaction.paddr[31:0] =  vifApbMaster.paddr[31:0];
-	coApbTransaction.pstrb[3:0]  =  vifApbMaster.pstrb[3:0];
-	coApbTransaction.pwrite      =  vifApbMaster.pwrite;
-	
-	do begin
-		repeat(1) @(posedge vifApbMaster.pclk);
-		$display ("[UVM_DEBUG]-----------come here first");
-		if(vifApbMaster.penable == 1 && vifApbMaster.pready == 1) begin	
-		$display ("[UVM_DEBUG]-----------come here");
-			if(vifApbMaster.pwrite == 1) begin
-			coApbTransaction.pwdata[31:0] =  vifApbMaster.pwdata[31:0];
-			end
-			else begin
-			coApbTransaction.prdata[31:0] =  vifApbMaster.prdata[31:0];
-			end
-			
-			if(coApbTransaction.paddr[31:0] == 32'h04 && coApbTransaction.pwdata[31:0] == 32'h01) begin
-				//TODO: compile error - wr_data_reg khong co trong class coApbTransaction
-				//coApbTransaction.wr_data_reg = 1'b1;
-			end
-			else if (coApbTransaction.paddr[31:0] == 32'h04 && coApbTransaction.pwdata[31:0] == 32'h00) begin
-				//TODO: compile error - wr_data_reg khong co trong class coApbTransaction
-				//coApbTransaction.wr_data_reg = 1'b0;
-			end 
-		end
-	end while(vifApbMaster.penable == 0 && vifApbMaster.pready == 0);
+	wait(vifApbMaster.psel && vifApbMaster.penable && vifApbMaster.pready)
+    //wait(vifApbMaster.psel);
+	//coApbTransaction.paddr[31:0] =  vifApbMaster.paddr[31:0];
+	//coApbTransaction.pstrb[3:0] = vifApbMaster.pstrb[3:0];
+    //coApbTransaction.pwrite = vifApbMaster.pwrite;
+	//do begin
+        repeat(1) @(posedge vifApbMaster.pclk) begin
+        $display("VietHT UVM ----- DEBUG ---- 1 --- penable %1h --- pready %1h --- time: %t",vifApbMaster.penable,vifApbMaster.pready,$time);
+
+             coApbTransaction.paddr[31:0] =  vifApbMaster.paddr[31:0];
+             coApbTransaction.pstrb[3:0] = vifApbMaster.pstrb[3:0];
+             coApbTransaction.pwrite = vifApbMaster.pwrite;
+             
+             if(vifApbMaster.penable == 1 && vifApbMaster.pready == 1) begin
+            $display("VietHT UVM ----- DEBUG ---- 2 --- penable %1h --- pready %1h --- time: %t",vifApbMaster.penable,vifApbMaster.pready,$time);
+                if(coApbTransaction.pwrite == 1) begin
+                coApbTransaction.pwdata[31:0] =  vifApbMaster.pwdata[31:0];
+                end
+                else begin
+                coApbTransaction.prdata[31:0] =  vifApbMaster.prdata[31:0];
+                end
+                
+                if(coApbTransaction.paddr[31:0] == 32'h04 && coApbTransaction.pwdata[31:0] == 32'h01) begin
+                    //TODO: compile error - wr_data_reg khong co trong class coApbTransaction
+                    //coApbTransaction.wr_data_reg = 1'b1;
+                end
+                else if (coApbTransaction.paddr[31:0] == 32'h04 && coApbTransaction.pwdata[31:0] == 32'h00) begin
+                    //TODO: compile error - wr_data_reg khong co trong class coApbTransaction
+                    //coApbTransaction.wr_data_reg = 1'b0;
+                end 
+            end
+        end
+	//end while(vifApbMaster.penable == 0 && vifApbMaster.pready == 0);
+    //end while(vifApbMaster.psel == 0);
 // Viet
 		ap_toScoreboard.write(coApbTransaction);
 	end
