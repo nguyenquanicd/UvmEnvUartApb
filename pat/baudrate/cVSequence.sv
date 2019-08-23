a1c43ca56964978d26f27535b8e9963cf1368849
nguyensinhton9x <35450836+nguyensinhton9x@users.noreply.github.com>
2019-08-24 01:32:12 +0700

    check baud rate

---
+++ pat/baudrate/cVSequence.sv
@@ -0,0 +1,48 @@
+//--------------------------------------
+//Project: The UVM environemnt for UART (Universal Asynchronous Receiver Transmitter)
+//Function: User UVM Sequence - This is the TEST PATTERN created by user
+//  - User modifty this class to create the expected transactions for the test purpose
+//Author:  Pham Thanh Tram, Nguyen Sinh Ton, Doan Duc Hoang, Truong Cong Hoang Viet, Nguyen Hung Quan
+//Page:    VLSI Technology
+//--------------------------------------
+
+class cVSequence extends uvm_sequence#(cApbTransaction);
+  //Register to Factory
+	`uvm_object_utils(cVSequence)
+  `uvm_declare_p_sequencer(cVSequencer)
+  
+  cApbMasterWriteSeq WriteSeq;
+  cApbMasterReadSeq ReadSeq;
+  // Object must not have veriable "parent" (refer to class cVSequencer)
+	function new (string name = "cVSequence");
+		super.new(name);
+	endfunction
+  //TEST PATTERN is written at here
+  task body();
+    #50ns
+    //--------------------------------------------
+    //Setting UART-TX (uart_0)
+    //--------------------------------------------
+    //Set baud rate
+    `ApbWriteTX(32'h00000008,32'h00000040) 
+   
+    //Enable UART TX
+    `ApbWriteTX(32'h00000004,32'h00000001) // Enable
+    
+    //--------------------------------------------
+    //Setting UART-RX (uart_1)
+    //--------------------------------------------
+    //Set baud rate
+    `ApbWriteRX(32'h00000008,32'h0000005A) 
+    
+    //Enable UART TX
+    `ApbWriteRX(32'h00000004,32'h00000001) 
+
+    //`ApbReadRX(32'h00000004, 32'h00000001, 32'hFFFFFFFF) //address, expected value, mask
+    //
+    //Write to DATA register of UART-TX to send data
+    //Note: DATA only is 8-bit LSB
+    `ApbWriteTX(32'h0000000C,32'h00000050)
+    `ApbReadRX(32'h0000000C,32'h00000050,32'hFFFFFFFF)
+  endtask
+endclass
