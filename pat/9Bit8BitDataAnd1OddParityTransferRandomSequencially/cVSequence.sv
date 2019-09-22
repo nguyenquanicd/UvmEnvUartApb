c8cd12f09f99e26450b573dcf87b4de5b6ee5dce
hoangviet81193 <53632249+hoangviet81193@users.noreply.github.com>
2019-09-22 16:34:24 +0700

    Add files via upload

--- pat/9Bit8BitDataAnd1OddParityTransferRandomSequencially/cVSequence.sv
+++ pat/9Bit8BitDataAnd1OddParityTransferRandomSequencially/cVSequence.sv
@@ -14,13 +14,15 @@ class cVSequence extends uvm_sequence#(cApbTransaction);
   //logic [31:0] exptData;
   cApbMasterWriteSeq WriteSeq;
   cApbMasterReadSeq ReadSeq;
-  cApbMasterWriteSeqNotCmpr ReadSeqWoCmpr;
+  cApbMasterReadSeqNotCmpr ReadSeqWoCmpr;
+  randc logic [7:0] data8b;
   // Object must not have veriable "parent" (refer to class cVSequencer)
 	function new (string name = "cVSequence");
 		super.new(name);
 	endfunction
   //TEST PATTERN is written at here
   task body();
+  logic [31:0] data;
     #50ns
     //--------------------------------------------
     //Setting UART-TX (uart_0)
@@ -41,9 +43,9 @@ class cVSequence extends uvm_sequence#(cApbTransaction);
     `ApbWriteRX(32'h00000004,32'h00000003) // parity bit is 0
     `ApbReadRX(32'h00000004,32'h00000023,32'h000000FF)
     
-    //RXFIFO đang chứa lớn hơn hoặc bằng 4 dữ liệu thì bit RXF của thanh ghi IR sẽ tích cực
-    `ApbWriteRX(32'h00000000,32'h00000008) // parity bit is 0
-    `ApbReadRX(32'h00000000,32'h00000008,32'h000000FF)
+    //RXFIFO đang chứa lớn hơn hoặc bằng 16 dữ liệu thì bit RXF của thanh ghi IR sẽ tích cực
+    `ApbWriteRX(32'h00000000,32'h00000000) // parity bit is 0
+    `ApbReadRX(32'h00000000,32'h00000000,32'h000000FF)
     //`ApbReadRX(32'h00000004, 32'h00000001, 32'hFFFFFFFF) //address, expected value, mask
     //
     //Write to DATA register of UART-TX to send data
@@ -58,27 +60,35 @@ class cVSequence extends uvm_sequence#(cApbTransaction);
     //        $stop;
     //    end
     //end
-    
-    for(int i = 0; i <= 3; i++) begin
-        `ApbWriteRandTX(32'h0000000C)
-         ExpDataArray[i] = WriteSeq.data;
-         $display("ExpDataArray[%d] cpr %h",i,WriteSeq.data);
-         end
-        //`ApbReadRX(32'h00000004,32'h000000A3,32'h000000ff)
-    while(1) begin
-         `ApbReadWoCmprRX(32'h00000014)
-         //if(ReadSeqWoCmpr.coApbTransaction.prdata[6] && !ReadSeqWoCmpr.coApbTransaction.prdata[7]) begin
-         if(ReadSeqWoCmpr.coApbTransaction.prdata[1]) begin
-         break;
-         end   
-     end
-     
-    for (int i = 0; i <= 3; i++) begin
-     `ApbReadRX(32'h0000000C,ExpDataArray[i],32'h000000ff)
-     $display("ExpDataArray[%d] cpr ---- %h",i,ExpDataArray[i]);
-     #100ns;
-    end 
-         
+    //logic [31:0] data;
+    for (int j = 0; j < 16; j++) begin
+        for (int i = 0; i < 16; i++) begin
+            assert (randomize(data8b) with (data8b<=8'hFF; data>=8'h00;});
+            data = 32'h00000000 | data8b;
+            //`ApbWriteRandTX(32'h0000000C);
+            `ApbWriteTX(32'h0000000C,data);
+            //ExpDataArray[i] = WriteSeq.data;         
+            //$display("ExpDataArray[%d] Write %h, j = %d, data = %h, datab = %h",i,WriteSeq.data, j, data, data8b);
+            $display("ExpDataArray[%d] Write %h, j = %d",i,WriteSeq.data, j);
+            #200ns;
+            end
+            //`ApbReadRX(32'h00000004,32'h000000A3,32'h000000ff)
+        while(1) begin
+            `ApbReadWoCmprRX(32'h00000014)
+            //if(ReadSeqWoCmpr.coApbTransaction.prdata[6] && !ReadSeqWoCmpr.coApbTransaction.prdata[7]) begin
+            if(ReadSeqWoCmpr.coApbTransaction.prdata[1]) begin
+            break;
+            end   
+        end
+        
+        for (int i = 0; i < 4; i++) begin
+        `ApbReadRX(32'h0000000C,ExpDataArray[i],32'h000000ff)
+        //$display("ExpDataArray[%d] Read cpr ---- %h, j = %d",i,ExpDataArray[i], j);
+        $display("ReadSeq.coApbTransaction.prdata ---- %h",ReadSeq.coApbTransaction.prdata);
+        #100ns;
+        end 
+        ExpDataArray.delete();
+    end     
 
     $stop;
 
